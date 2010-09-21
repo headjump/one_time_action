bc198e27da41fdc7e1da1e8b630d825070115b1d
Dennis Treder <info@dennistreder.de>
2010-09-21 21:54:58 +0200

    added base files

---
+++ src/jquery.one_time_action.js
@@ -0,0 +1,11 @@
+/*!
+ * one_time_action JavaScript plugin for jQuery
+ * http://github.com/headjump/one-time-action
+ *
+ * Copyright 2010, Dennis Treder
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ */
+
+(function() {
+  
+})(jQuery);
